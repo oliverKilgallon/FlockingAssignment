ec874fd84eb87e6a522529845cc4b59335563179
O.T.Kilgallon U1654523 <U1654523@unimail.hud.ac.uk>
2018-02-05 12:04:18 +0000

    Adding current state of assignment

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float m_MoveSpeed;
+    public float m_RotSpeed;
+	
+	void Update ()
+    {
+        MovementUpdate();	
+	}
+
+    void MovementUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.position += transform.forward * moveVertical * m_MoveSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up, moveHorizontal * m_RotSpeed);
+    }
+    
+}
